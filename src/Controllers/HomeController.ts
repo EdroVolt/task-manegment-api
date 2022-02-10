ef68da8487a4c14f5730a1552aa52c2d43c7a925
Ahmed Mohamed Abo Edrees <ahmededrees30@gmail.com>
2022-02-10 20:23:22 +0200

    feat: add link ti github repo for docs

--- src/Controllers/HomeController.ts
+++ src/Controllers/HomeController.ts
@@ -3,7 +3,10 @@ import { Request, Response } from "express";
 export class HomeController {
   WelcomScreen(req: Request, res: Response) {
     return res.send(`<h1>Welcom To our Task Manegment API</h1>
-      <p style="font-size: 22px; fon-family: arial;">Click this <a href="#">link</a> to read the docs If you don't know how to use this API </p>
+      <p style="font-size: 22px; fon-family: arial;">
+        Click this 
+        <a href="https://github.com/EdroVolt/task-manegment-api">link</a>
+        to read the docs If you don't know how to use this API </p>
       `);
   }
 }
